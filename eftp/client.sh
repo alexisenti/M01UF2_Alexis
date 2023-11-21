7082b17670508f4459ff779fef688c1119924049
enti <enti@alexis.enti>
2023-11-21 04:08:39 -0500

    Primeros pasos en el cliente/servidor del protocolo EFTP

---
+++ eftp/client.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "CLIENTE DE EFTP"
+
+echo "(1) Send"
+
+echo "EFTP 1.0" | nc LOCALHOST 3333
+
+echo "(2) Listen"
+
+DATA=`nc -l -p 3333 -w 0`
+
+echo $DATA
+
+echo "(5) Test & Send"
+
+if [ "$DATA" != "OK_HEADER" ] #paso 5
+then
+	echo "ERROR 1: BAD HEADER"
+	exit 1
+fi
+
+echo "BOOM"
+sleep 1
+echo "BOOM" | nc LOCALHOST 3333
+
+echo "(6) Listen"
+
+DATA=`nc -l -p 3333 -w 0`
+echo $DATA
---
+++ eftp/server.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+CLIENT="LOCALHOST"
+
+echo “Servidor de EFTP”
+
+echo “(0) Listen”
+
+DATA=`nc -l -p 3333 -w 0`
+
+echo $DATA
+
+echo “(3) Test and Send” #comprobar si lo que ha llegado es igual a la cabecera de “eftp 1.0”
+
+if [”$DATA” != “EFTP 1.0” ]
+
+then 
+
+	echo “ERROR 1: BAD HEADER”
+	sleep 1
+	echo “KO_HEADER” | nc $CLIENT 3333
+	exit 1
+
+fi
+
+echo “OK_HEADER”
+
+sleep 1
+
+echo “OK_HEADER” | nc $CLIENT 3333
+
+echo “(4) Listen”
+
+DATA=`nc -l -p 3333 -w 0` #paso 4 escuchar
+echo $DATA
+
+echo “(7) Test and Send”
+
+if [”$DATA” != “BOOM” ]
+then
+	echo “ERROR 1: BAD HEADER”
+	sleep 1
+	echo “KO_HEADER” | nc $CLIENT 3333
+	exit 1
+
+fi
+
+echo "OK_HANDSHAKE"
+sleep 1
+echo "OK_HANDSHAKE" | nc $CLIENT 3333
+
+echo "(8) Listen"
+
+
+DATA=`nc -l -p 3333 -w 0`
+
+
